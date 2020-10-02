2d2d96fed8752d6d8b05414ce5c79ff95097db75
Kevin Disneur <kevin@disneur.me>
2020-10-02 22:16:39 +0200

    Add all necessary files

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	fmt.Println("Hello World")
+
+	return nil
+}
